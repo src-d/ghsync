d25607f1210c9b77f8fbbdd95b9b722086c0d39a
Carlos Martín <carlos.martin.sanchez@gmail.com>
2019-06-06 17:02:25 +0100

    Use cmd options for configuration
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

--- cmd/ghsync/main.go
+++ cmd/ghsync/main.go
@@ -5,14 +5,13 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
-	"os"
+
+	"github.com/src-d/ghsync"
+	"github.com/src-d/ghsync/utils"
 
 	"github.com/google/go-github/github"
 	"github.com/gregjones/httpcache"
 	"github.com/gregjones/httpcache/diskcache"
-
-	"github.com/src-d/ghsync"
-	"github.com/src-d/ghsync/utils"
 	"golang.org/x/oauth2"
 	"gopkg.in/src-d/go-cli.v0"
 	"gopkg.in/src-d/go-queue.v1"
@@ -36,42 +35,66 @@ func main() {
 
 type syncCommand struct {
 	cli.Command `name:"sync"`
+
+	Token string `long:"token" env:"GHSYNC_TOKEN" description:"GitHub personal access token" required:"true"`
+	Org   string `long:"org" env:"GHSYNC_ORG" description:"Name of the GitHub organization" required:"true"`
+
+	QueueOpt struct {
+		Queue  string `long:"queue" env:"GHSYNC_QUEUE" description:"queue name. If it's not set the organization name will be used"`
+		Broker string `long:"broker" env:"GHSYNC_BROKER" default:"amqp://localhost:5672" description:"broker service URI"`
+	} `group:"go-queue connection options"`
+
+	PostgresOpt struct {
+		DB       string `long:"postgres-db" env:"GHSYNC_POSTGRES_DB" description:"PostgreSQL DB" default:"ghsync"`
+		User     string `long:"postgres-user" env:"GHSYNC_POSTGRES_USER" description:"PostgreSQL user" default:"superset"`
+		Password string `long:"postgres-password" env:"GHSYNC_POSTGRES_PASSWORD" description:"PostgreSQL password" default:"superset"`
+		Host     string `long:"postgres-host" env:"GHSYNC_POSTGRES_HOST" description:"PostgreSQL host" default:"localhost"`
+		Port     int    `long:"postgres-port" env:"GHSYNC_POSTGRES_PORT" description:"PostgreSQL port" default:"5432"`
+	} `group:"PostgreSQL connection options"`
 }
 
 func (c *syncCommand) Execute(args []string) error {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		"localhost", 5432, "superset", "superset", "ghsync")
+		c.PostgresOpt.Host, c.PostgresOpt.Port, c.PostgresOpt.User,
+		c.PostgresOpt.Password, c.PostgresOpt.DB)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	org := os.Getenv("GITHUB_ORG")
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
 
 	http := oauth2.NewClient(context.TODO(), oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+		&oauth2.Token{AccessToken: c.Token},
 	))
 
-	t := httpcache.NewTransport(diskcache.New("cache/" + org))
+	t := httpcache.NewTransport(diskcache.New("cache/" + c.Org))
 	t.Transport = &RemoveHeaderTransport{utils.NewRateLimitTransport(http.Transport)}
 	http.Transport = t
 
 	client := github.NewClient(http)
 
-	broker, err := queue.NewBroker("amqp://localhost:5672")
+	broker, err := queue.NewBroker(c.QueueOpt.Broker)
 	if err != nil {
 		return err
 	}
 
-	queue, err := broker.Queue(org)
+	qName := c.QueueOpt.Queue
+	if qName == "" {
+		qName = c.Org
+	}
+	queue, err := broker.Queue(qName)
 	if err != nil {
 		return err
 	}
 
 	syncer := ghsync.NewSyncer(db, client, queue)
-	go syncer.DoOrganization(org)
+	go syncer.DoOrganization(c.Org)
 	fmt.Println(syncer.Wait())
 
 	return nil
